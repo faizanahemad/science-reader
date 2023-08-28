6a42764bdfe9925783d503eaad2702edb8c42f66
Faizan Ahemad <fahemad3@gmail.com>
2023-08-28 23:44:36 +0530

    Usability Fixes

--- server.py
+++ server.py
@@ -987,7 +987,9 @@ def cached_get_file(file_url):
     else:   
         file_data = []
         try:
-            req = requests.get(file_url, stream=True)
+            headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
+            req = requests.get(file_url, stream=True,
+                               verify=False, headers=headers)
         except requests.exceptions.RequestException as e:
             logger.error(f"Failed to download file: {e}")
             req = requests.get(file_url, stream=True, verify=False)
