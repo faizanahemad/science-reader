7d9f03e0def82ddd25b73fc8d5fc47c92ae8bf58
faizanahemad <ahemf@amazon.com>
2024-03-27 16:02:50 +0530

    Default as stateless

--- interface/chat.js
+++ interface/chat.js
@@ -364,7 +364,7 @@ var ChatManager = {
                 showPDF(doc.source, "chat-pdf-content");
                 $("#chat-pdf-content").removeClass('d-none');
             });
-            
+
             
             // Attach delete event
             deleteDiv.click(function(event) {
@@ -526,7 +526,7 @@ function loadConversations(autoselect=true) {
             conversationItem.append(deleteButton);
 
             // Add a button for conversation state
-            var stateButton = $('<small><button class="btn p-0 ms-2 stateless-button"><i class="bi bi-eye-slash"></i></button></small>');
+            var stateButton = $('<small><button class="btn p-0 ms-2 stateless-button"><i class="bi bi-eye"></i></button></small>');
             conversationItem.append(stateButton);
             
             // Include a summary of the conversation
