25e93113bb3e36d9219a1ac3b0ed153088dae84c
faizanahemad <ahemf@amazon.com>
2024-04-26 09:33:37 +0530

    Multi-domain

--- test_serp.py
+++ test_serp.py
@@ -4,7 +4,7 @@ from pprint import pprint
 import time
 def serp():
     query = "coffee mugs"
-    key = "f6b1b5668e05db3f400e798ffebad2a26c107ef3a97b48d11f9bbb38ea5d90f5"
+    key = None
     num = 10
     start_time = time.time()
     results = serpapi(query, key, num, our_datetime=None, only_pdf=False, only_science_sites=False)
@@ -15,7 +15,7 @@ def serp():
     pprint(results[0].keys())
     print("Time taken: ", f"{(end_time - start_time):.2f}")
 
-    key = {"cx": "f47a96c4f436c40ad", "api_key": "AIzaSyBsCe-8MeLWn0AJGpxg6tszc4Dz3Y-LQ2Q"}
+    key = None
     start_time = time.time()
     results = googleapi(query, key, num, our_datetime=None, only_pdf=False, only_science_sites=False)
     end_time = time.time()
