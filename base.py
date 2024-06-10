5c2e969ed5a67640eb0f451d9ee5e68339d1f2cf
faizanahemad <ahemf@amazon.com>
2024-06-10 20:39:35 +0530

    Bug fix

--- base.py
+++ base.py
@@ -66,7 +66,8 @@ openai_rate_limits = defaultdict(lambda: (1000000, 10000), {
 
 openai_model_family = {
     "gpt-3.5-turbo": ["gpt-3.5-turbo", "gpt-3.5-turbo-0301", "gpt-3.5-turbo-0613", "gpt-3.5-turbo-1106"],
-    "gpt-3.5-16k": ["gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613", "gpt-3.5-turbo-1106"],
+    "gpt-3.5-16k": ["gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613"],
+    "gpt-3.5-turbo-16k": ["gpt-3.5-turbo-16k", "gpt-3.5-turbo-16k-0613"],
     "gpt-3.5-turbo-instruct": ["gpt-3.5-turbo-instruct", "gpt-3.5-turbo-instruct-0914"],
     "gpt-4": ["gpt-4", "gpt-4-0314", "gpt-4-0613", "gpt-4-32k-0314"],
     "gpt-4-turbo": ["gpt-4-turbo", "gpt-4-vision-preview"],
